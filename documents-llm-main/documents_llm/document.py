37b746c74289d9c6b85aa63b01f46efb963fd896
maanasphiscape <maanasphiscape@gmail.com>
2025-01-17 10:23:48 +0530

    Update document.py

--- documents-llm-main/documents_llm/document.py
+++ documents-llm-main/documents_llm/document.py
@@ -1,32 +1,6 @@
-from pathlib import Path
+from langchain_community.document_loaders.pdf import PyPDFLoader
 
-#from langchain_community.document_loaders.pdf import PyPDFLoader
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-try:
-    from langchain_community.document_loaders.pdf import PyPDFLoader
-except ImportError as e:
-    logging.error("Error importing PyPDFLoader from langchain_community: %s", e)
-    raise
-
-from langchain_community.document_loaders.text import TextLoader
-from langchain_core.documents.base import Document
-
-
-def load_pdf(
-    file_path: Path | str, start_page: int = 0, end_page: int = -1
-) -> list[Document]:
-    print(f"Loading PDF: {file_path}, start_page: {start_page}, end_page: {end_page}")
-    loader = PyPDFLoader(str(file_path))
-    return (
-        loader.load()[start_page:end_page]
-        if end_page != -1
-        else loader.load()[start_page:]
-    )
-
-
-def load_text(file_path: Path | str) -> list[Document]:
-    loader = TextLoader(str(file_path))
+def load_pdf(file):
+    """Loads and extracts content from a PDF file."""
+    loader = PyPDFLoader(file)
     return loader.load()
